8208b4de54976a6aea06535aa2628f71673edc77
Albert Wu <38256449+CrazyAncestor@users.noreply.github.com>
2024-12-14 19:49:27 -0600

    Add files via upload

--- Q_rep_PhotonDissipation.py
+++ Q_rep_PhotonDissipation.py
@@ -3,36 +3,40 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm  # Import tqdm for progress bar
 import os  # For handling directories
 from fokker_planck_simulator import FokkerPlanckSimulator
+import sympy as sp
 
 # RK4 Solver
-def rk4_step(t, y, dt, phys_parameter):
+def rk4_step(t, y, dt, phys_parameter, time_deriv_funcs):
     # Get the k1, k2, k3, k4 slopes
-    k1 = dt* system(t, y, phys_parameter)
-    k2 = dt* system(t + dt/2, y + k1/2, phys_parameter)
-    k3 = dt* system(t + dt/2, y + k2/2, phys_parameter)
-    k4 = dt* system(t + dt, y + k3, phys_parameter)
+    k1 = dt* system(t, y, phys_parameter, time_deriv_funcs)
+    k2 = dt* system(t + dt/2, y + k1/2, phys_parameter, time_deriv_funcs)
+    k3 = dt* system(t + dt/2, y + k2/2, phys_parameter, time_deriv_funcs)
+    k4 = dt* system(t + dt, y + k3, phys_parameter, time_deriv_funcs)
     
     # Update the solution using the weighted average
     return y + (k1 + 2*k2 + 2*k3 + k4) / 6
 
 
 # Define the system of differential equations
-def system(t, y, phys_parameter):
-    gamma, nu, n_th, eps2, x0, y0 =  phys_parameter
-    a, b, c, d, e = y
-    a_prime = (gamma*(2*d + 2*e + 2*d*n_th + 2*e*n_th + b**2*n_th + c**2*n_th + b**2 + c**2 + 4))/4
-    b_prime = (b*gamma*(2*d + 2*d*n_th + 1))/2
-    c_prime = (c*gamma*(2*e + 2*e*n_th + 1))/2
-    d_prime = gamma*(n_th + 1)*d**2 + gamma*d
-    e_prime = gamma*(n_th + 1)*e**2 + gamma*e
-
-    return np.array([a_prime, b_prime, c_prime, d_prime, e_prime])
+def system(t, y, phys_parameter, time_deriv_funcs):
+    gamma0, nu0, n_th0, eps2, x0, y0 =  phys_parameter
+    a0, b0, c0, d0, e0, f0 = y
+    a_prime, b_prime, c_prime, d_prime, e_prime, f_prime = time_deriv_funcs
+    
+    a_prime0 = a_prime(a0,b0,c0,d0,e0,f0,gamma0,nu0,n_th0)
+    b_prime0 = b_prime(a0,b0,c0,d0,e0,f0,gamma0,nu0,n_th0)
+    c_prime0 = c_prime(a0,b0,c0,d0,e0,f0,gamma0,nu0,n_th0)
+    d_prime0 = d_prime(a0,b0,c0,d0,e0,f0,gamma0,nu0,n_th0)
+    e_prime0 = e_prime(a0,b0,c0,d0,e0,f0,gamma0,nu0,n_th0)
+    f_prime0 = f_prime(a0,b0,c0,d0,e0,f0,gamma0,nu0,n_th0)
+    
+    return np.array([a_prime0, b_prime0, c_prime0, d_prime0, e_prime0, f_prime0])
 
 # Plotting complex representation function with parameters
 def ProbDensMap(x, y, solution):
     X, Y = np.meshgrid(x, y)
-    a, b, c, d, e = solution
-    ProbDens = np.exp(a + b * X + c * Y + d * X**2 + e * Y**2)
+    a, b, c, d, e, f = solution
+    ProbDens = np.exp(a + b * X + c * Y + d * X**2 + e * Y**2 + f * X* Y)
     return ProbDens
 
 """# Analytical solution function
@@ -69,7 +73,8 @@ b0 = 2 * x0 /eps2  # Initial condition for b
 c0 = 2 * y0 /eps2  # Initial condition for c
 d0 = - 1/eps2  # Initial condition for d
 e0 = - 1/eps2  # Initial condition for e
-init_cond = np.array([a0, b0, c0, d0, e0])
+f0 = 0
+init_cond = np.array([a0, b0, c0, d0, e0, f0])
 
 #   Time parameter
 t_start = 0
@@ -83,6 +88,8 @@ y = np.linspace(-5, 5, 100)
 output_dir = "Q_rep_PhotonDissipation"
 
 # Instantiate and run the simulation
+
+
 simulator = FokkerPlanckSimulator(t_start, t_end, dt, x, y, phys_parameter, init_cond, output_dir, ProbDensMap, rk4_step)
-simulator.run_simulation(pure_parameter = False)
-simulator.electric_field_evolution(representation='Q')
+simulator.run_simulation(pure_parameter = True)
+"""simulator.electric_field_evolution(representation='Q')"""
--- fokker_planck_simulator.py
+++ fokker_planck_simulator.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import os
 from tqdm import tqdm
 import cmath
+import sympy as sp
 
 class FokkerPlanckSimulator:
     def __init__(self, t_start, t_end, dt, x, y, phys_parameter, init_cond, output_dir, ProbDensMap, solver, analytical=None):
@@ -41,6 +42,97 @@ class FokkerPlanckSimulator:
 
         os.makedirs(self.output_dir, exist_ok=True)
 
+        
+        x_var, y_var, a, b, c, d, e, f = sp.symbols('x_var y_var a b c d e f')
+
+        def solve_fkpk_symbol(PT_model,x, y, a, b, c, d, e, f):
+            # 定義符號變數
+            
+
+            # 定義函數 P(x, y)
+            P = sp.exp(a + b*x + c*y + d*x**2 + e*y**2 + f*x*y)
+
+            # 一階導數
+            def fst_deriv(func, var):
+                return sp.simplify(sp.diff(func, var))
+
+            # 二階導數
+            def snd_deriv(func, var):
+                return sp.simplify(sp.diff(func, var, var))
+
+            # 混合二階導數
+            def diff_deriv(func, var1, var2):
+                return sp.simplify(sp.diff(sp.diff(func, var1), var2))
+
+            # 指數函數的一階導數
+            def exp_fst_deriv(func, var):
+                return sp.simplify(sp.diff(func, var) / func)
+
+            # 指數函數的二階導數
+            def exp_snd_deriv(func, var):
+                return sp.simplify(sp.diff(sp.diff(func, var), var) / func)
+
+            # 指數函數的混合二階導數
+            def exp_diff_deriv(func, var1, var2):
+                return sp.simplify(sp.diff(sp.diff(func, var1), var2) / func)
+
+            # 計算 P 的一階和二階導數
+            def fst_snd_derivs(P, x, y):
+                Px = exp_fst_deriv(P, x)
+                Py = exp_fst_deriv(P, y)
+
+                Pxx = exp_snd_deriv(P, x)
+                Pyy = exp_snd_deriv(P, y)
+
+                Pxy = exp_diff_deriv(P,x,y)
+                
+                return Px, Py, Pxx, Pyy, Pxy
+
+            # 計算對 x 和 y 的導數
+            Px, Py, Pxx, Pyy, Pxy = fst_snd_derivs(P, x, y)
+
+            # 計算 P 的時間導數
+            PT = PT_model(x,y,Px,Py,Pxx,Pyy,Pxy)
+
+            # 提取係數的函數
+            def extract_coef(PT, x, y):
+                PTxx = snd_deriv(PT, x)
+                PTyy = snd_deriv(PT, y)
+                PTxy = diff_deriv(PT,x,y)
+
+                PTx = sp.simplify(fst_deriv(PT, x) - (PTxx * x) - (PTxy * y))
+                PTy = sp.simplify(fst_deriv(PT, y) - (PTyy * y) - (PTxy * x))
+
+                PT_const = PT - ((PTxx * x**2 + PTyy * y**2) / 2 + PTxy * x * y + (PTx * x + PTy * y))
+                PT_const = sp.simplify(PT_const)
+
+                PTxx = PTxx / 2
+                PTyy = PTyy / 2
+                
+                return PT_const, PTx, PTy, PTxx, PTyy, PTxy
+
+            # 提取時間導數的各項係數
+            
+            PT_const, PTx, PTy, PTxx, PTyy, PTxy = extract_coef(PT, x, y)
+            return PT_const, PTx, PTy, PTxx, PTyy, PTxy
+        gamma_var, nu_var, n_th_var = sp.symbols('gamma_var nu_var n_th_var')
+
+        # Use exponential function to simulate the solution of Fokker-Planck eq.
+        def PT_model_QREP(x,y,Px,Py,Pxx,Pyy,Pxy):
+            PT1 = gamma_var + (gamma_var / 2 * x) * Px + (gamma_var / 2 * y) * Py
+            PT2 = gamma_var * (n_th_var + 1) / 4 * (Pxx + Pyy)
+            return PT1 + PT2
+        
+        PT_const, PTx, PTy, PTxx, PTyy, PTxy = solve_fkpk_symbol(PT_model_QREP,x_var, y_var, a, b, c, d, e, f)
+
+        a_prime = sp.lambdify((a,b,c,d,e,f,gamma_var,nu_var,n_th_var), PT_const, 'numpy')
+        b_prime = sp.lambdify((a,b,c,d,e,f,gamma_var,nu_var,n_th_var), PTx, 'numpy')
+        c_prime = sp.lambdify((a,b,c,d,e,f,gamma_var,nu_var,n_th_var), PTy, 'numpy')
+        d_prime = sp.lambdify((a,b,c,d,e,f,gamma_var,nu_var,n_th_var), PTxx, 'numpy')
+        e_prime = sp.lambdify((a,b,c,d,e,f,gamma_var,nu_var,n_th_var), PTyy, 'numpy')
+        f_prime = sp.lambdify((a,b,c,d,e,f,gamma_var,nu_var,n_th_var), PTxy, 'numpy')
+        time_deriv_funcs = a_prime, b_prime, c_prime, d_prime, e_prime, f_prime
+
         # Open a log file to write simulation messages
         log_filename = os.path.join(self.output_dir, "simulation_log.txt")
         with open(log_filename, 'w') as log_file:
@@ -49,7 +141,7 @@ class FokkerPlanckSimulator:
             if pure_parameter:
                 log_file.write("Running with pure_parameter mode enabled.\n")
                 for t in tqdm(range(1, self.nsteps), desc="Simulating", unit="step"):
-                    self.solution[t] = self.solver(self.t_vals[t-1], self.solution[t-1], self.dt, self.phys_parameter)
+                    self.solution[t] = self.solver(self.t_vals[t-1], self.solution[t-1], self.dt, self.phys_parameter, time_deriv_funcs)
                 # Plot the evolution of a(t), b(t), c(t), d(t) over time
                 self.plot_parameter_evolution()
                 log_file.write("Simulation completed.\n")
@@ -65,7 +157,7 @@ class FokkerPlanckSimulator:
                 
                 # Time integration using RK4 with progress bar
                 for t in tqdm(range(1, self.nsteps), desc="Simulating", unit="step"):
-                    self.solution[t] = self.solver(self.t_vals[t-1], self.solution[t-1], self.dt, self.phys_parameter)
+                    self.solution[t] = self.solver(self.t_vals[t-1], self.solution[t-1], self.dt, self.phys_parameter, time_deriv_funcs)
 
                     # Compute the Probability Distribution at the current time step
                     ProbDens = self.ProbDensMap(self.x, self.y, self.solution[t])
